f31806ccef55e4fba692bd063c13434b1450d0a0
Jordan Kubot <jjkww4@mst.edu>
2018-07-07 19:19:11 -0700

    player movement optimization

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour {
     public float moveForce = 0.0001f;
     public float maxSpeed = 0.00000001f;
     public float jumpForce = 0.4f;
+    public float friction = 0.1f;
     public Transform groundCheck;
 
 
@@ -25,13 +26,7 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, whatIsGround);
-        Debug.Log(grounded);
-
-        if (Input.GetAxis("Vertical") > 0 && grounded)
-        {
-            jump = true;
-        }
+        
     }
 
     void FixedUpdate()
@@ -40,11 +35,16 @@ public class PlayerMovement : MonoBehaviour {
 
         if (h * rb2d.velocity.x < maxSpeed)
             rb2d.AddForce(Vector2.right * h * moveForce);
-
+        
         if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
             rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
 
-        if (jump)
+        if (h == 0)
+            rb2d.velocity = new Vector2(rb2d.velocity.x * friction, rb2d.velocity.y);
+
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, whatIsGround);
+        Debug.Log(grounded);
+        if (Input.GetAxis("Vertical") > 0 && grounded && !jump && rb2d.velocity.y <= 0)
         {
             rb2d.AddForce(new Vector2(0f, jumpForce));
             jump = false;
